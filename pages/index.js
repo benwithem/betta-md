f503ce580be2e150614bf226ea0708f50b2fb54e
Ben Withem <benwithem@gmail.com>
2024-11-25 14:40:43 -0500

    basic app shell

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Betta-MD</h1>
+      <p>This is the home page of the Betta-MD application.</p>
+    </Layout>
+  );
+};
+
+export default Home;
