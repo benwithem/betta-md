78fbd405de86e5b5b6463d3133390c65e75f714d
Ben Withem <benwithem@gmail.com>
2024-11-25 14:51:09 -0500

    Removing old demo

---
+++ app/aquariums.tsx
@@ -0,0 +1,24 @@
+import Layout from '../app/Layout';
+import { Card, Text, Button, Group } from '@mantine/core';
+
+const AquariumTracker = () => {
+  return (
+    <Layout>
+      <Card shadow="sm" p="lg" radius="md" withBorder>
+        <Text weight={700} size="xl" mb="sm">
+          Aquarium Tracker
+        </Text>
+        <Text size="sm" color="dimmed">
+          Track your aquarium parameters and get dosing recommendations.
+        </Text>
+        <Group position="right" mt="md">
+          <Button variant="light" color="blue">
+            Add New Log
+          </Button>
+        </Group>
+      </Card>
+    </Layout>
+  );
+};
+
+export default AquariumTracker;
