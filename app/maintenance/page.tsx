14e8fc852c6df33801548391fd3c286eb6d132e4
Ben Withem <benwithem@gmail.com>
2024-11-26 16:01:50 -0500

    push

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import React from 'react';
+import { Container, Title, Stack } from '@mantine/core';
 import { MainLayout } from '../components/layout/MainLayout';
 import { MaintenanceForm } from '../components/maintinence/MaintenanceForm';
-import { Container, Title } from '@mantine/core';
 
 export default function MaintenancePage() {
   return (
     <MainLayout>
-      <Container size="lg" py="xl">
-        <Title order={1} mb="xl">Maintenance Log</Title>
-        <MaintenanceForm />
+      <Container size="lg">
+        <Stack>
+          <Title order={1}>Maintenance Log</Title>
+          <MaintenanceForm />
+        </Stack>
       </Container>
     </MainLayout>
   );
